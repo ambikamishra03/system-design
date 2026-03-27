2958d259cd5b866f9994f5f9702d143e250200d6
Ambika Mishra <ambikamishra9236@gmail.com>
2026-03-27 20:32:06 +0530

    encapsulation

--- encapsulation.cpp
+++ encapsulation.cpp
@@ -2,30 +2,42 @@
 #include<string>
 
 using namespace std;
-class Car{
-    public:
-    virtual void startEngine()=0;
-    virtual void shiftGear(int gear)=0;
-    virtual void accelerate()=0;
-    virtual void Break()=0;
-    virtual void stopEngine()=0;
-    virtual ~Car() {}
-};
 
-class SportsCar: public Car {
-public:
+
+// data and methods encapsulated in a class
+class SportsCar {
+private:
   string brand;
   string model;
   bool isEngineOn;
   int currentSpeed;
   int currentGear;
+  string tyre;
+  public:
   SportsCar(string a, string b){
     this->brand=a;
     this->model=b;
     isEngineOn=false;
     currentSpeed=0;
     currentGear=0;
+    tyre="MRF";
   }
+
+
+  // getters and setters 
+int getCurrentSpeed() {
+return this->currentSpeed;
+}
+int getCurrentGear() {
+return this->currentGear;
+}
+string getTyre(){
+  return this->tyre;
+}
+
+void setTyre(string tyre){
+  this->tyre=tyre;
+}
   void startEngine(){
    isEngineOn=true;
    cout<<brand<<" "<<model<<" "<<": Engine starts!"<<endl;
@@ -64,13 +76,19 @@ public:
 int main() {
 
     // object creation
-    Car* mycar= new SportsCar("ford", "mustang");
+    SportsCar* mycar= new SportsCar("ford", "mustang");
     mycar->startEngine();
     mycar->shiftGear(20);
     mycar->accelerate();
-    mycar->shiftGear(10);
+    mycar->shiftGear(30);
     mycar->accelerate();
     mycar->Break();
     mycar->stopEngine();
+
+    // prohibited - we cant change this so make all data to private to ensure data security.
+    // mycar->currentSpeed = 400;
+    // cout<<"current speed is "<<mycar<<endl;
+    cout<<mycar->getCurrentSpeed()<<endl;
+    delete mycar;
     return 0;
 }
